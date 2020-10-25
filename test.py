41973208c1ff8c1d50bb8d72f0025440f8fba127
yudiyang2009 <73065901+yudiyang2009@users.noreply.github.com>
2020-10-25 21:59:16 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+def is_passing_grade(grade):
+    '''(int)->str
+
+    return 'pass' if grade is at least 50 and return 'fail' otherwise.
+    
+    >>> is_passing_grade(45)
+    'fail'
+    >>> is_passing_grade(80.5)
+    'pass'
+    '''
+
+    return is_passing_grade(50.0)
+
+
